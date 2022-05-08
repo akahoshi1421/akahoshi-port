b9d89756a31a9506b97950036fbcb8724e33dde9
akahoshi1421 <akaho1421@gmail.com>
2022-05-08 22:31:58 +0900

    画面遷移のCSSを修正

--- js/load.js
+++ js/load.js
@@ -1,12 +1,20 @@
-const lines = '<div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>            <div class="title">                <p>LOADING</p>            </div>';
+const lines = '<div class="title">                <p>LOADING</p>            </div>';
 
+const pcHeight = window.screen.height;
+const pcWidth = window.screen.width;
 
 let loading = function(){
     let container = document.querySelector(".loader2");
     
     let a = document.createElement("div");
     a.className = "line";
-    a.innerHTML = lines
+    let insertalt = "";
+    for(let height = 0; height < pcHeight * 3; height += 70){
+        insertalt += lines;
+    }
+
+    a.innerHTML = insertalt;
+
     container.appendChild(a);
 }
 
