adea4824e3c18e4eddcd97f30cc2c4a14d208a6c
akahoshi1421 <akaho1421@gmail.com>
2022-05-16 20:56:18 +0900

    コンタクトページのUXを変更

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,6 @@
 $(document).on("click", ".to-foreign", function(){
     let to =  $(this).attr("to");
-
-    let loadiv = setInterval(loading, 50);
-    $(".loader-mask").addClass("loading");
-
-    setTimeout(function(){
-        clearInterval(loadiv);
-        window.location = to;
-    }, 1200);
+    window.open(to);
 });
 
 $(document).on("click", ".email", function(){
